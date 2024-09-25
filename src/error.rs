fddd06eeb7c3c24265f1fe03bf785b8165029911
tayu0110 <tayu.dev@gmail.com>
2024-09-26 00:57:53 +0900

    Add comments to `exml::error::generic_error_default`.

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
 use std::io::{Error, Write};
 
+/// Default generic error function.
+///
+/// If `out` is `None`, output `libc:perror`-like format message to stderr.
 pub fn generic_error_default(out: Option<&mut impl Write>, msg: &str) {
     if let Some(out) = out {
         write!(out, "{msg}").ok();
