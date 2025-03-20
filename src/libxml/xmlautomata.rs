1cffc37f84401914910a4606035ac7977e338eaf
tayu0110 <tayu.dev@gmail.com>
2025-03-20 23:21:32 +0900

    Fix the module level comments for `xmlautomata` and `xmlregexp`.

--- src/libxml/xmlautomata.rs
+++ src/libxml/xmlautomata.rs
@@ -1,6 +1,6 @@
-//! Provide methods and data structures for building regexp automata.  
-//! This module is based on `libxml/xmlautomata.h`, `xmlregexp.c`, and so on in `libxml2-v2.11.8`.
+//! Provide methods and data structures for building regexp automata.
 //!
+//! This module is based on `libxml/xmlautomata.h`, `xmlregexp.c`, and so on in `libxml2-v2.11.8`.  
 //! Please refer to original libxml2 documents also.
 
 // Copyright of the original code is the following.
